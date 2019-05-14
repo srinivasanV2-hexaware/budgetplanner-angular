0b040f754882991653217d082d57195bc8589514
srinivasan23121987 <ursfriendly.sri23@gmail.com>
2019-05-15 00:03:32 +0530

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,10 @@ import { EditBudgetComponent } from './edit-budget/edit-budget.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { HeaderComponent } from './header/header.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCardModule, MatToolbarModule, MatInputModule, MatProgressBarModule, MatListModule, MatIconModule, MatBottomSheetModule, MatStepperModule, MatFormFieldModule, MatTableModule, MatPaginatorModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatToolbarModule, MatInputModule, MatProgressBarModule, MatListModule, MatIconModule, MatBottomSheetModule, MatStepperModule, MatFormFieldModule, MatTableModule, MatPaginatorModule, MatGridListModule } from '@angular/material';
 import { NotfoundComponent } from './notfound/notfound.component';
+import { ChartsModule } from 'ng2-charts';
+
 import { HomeComponent } from './home/home.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
@@ -38,7 +40,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MatInputModule,
     MatTableModule,
     MatPaginatorModule,
-    MatCardModule
+    MatCardModule,
+    MatGridListModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/create-budget/create-budget.component.ts
+++ src/app/create-budget/create-budget.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { ChartType, ChartOptions } from 'chart.js';
+import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip } from 'ng2-charts';
 @Component({
   selector: 'app-create-budget',
   templateUrl: './create-budget.component.html',
@@ -9,22 +11,131 @@ export class CreateBudgetComponent implements OnInit {
 
   isLinear = false;
   incomeFormGroup: FormGroup;
-  firstFormGroup: FormGroup;
+  housingFormGroup: FormGroup;
   secondFormGroup: FormGroup;
-
-  constructor(private _formBuilder: FormBuilder) {}
+  transportFormGroup: FormGroup;
+  educationalFormGroup: FormGroup;
+  personalFormGroup: FormGroup;
+  savingsFormGroup: FormGroup;
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public pieChartLabels: Label[] = ['Income', 'Housing', 'Transportation', 'Education', 'Personal', 'Savings'];
+  public pieChartData: SingleDataSet = [0, 0, 0, 0, 0, 0];
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = false;
+  public totalValue = 0;
+  public pieChartPlugins = [];
+  constructor(private _formBuilder: FormBuilder) {
+    monkeyPatchChartJsTooltip();
+    monkeyPatchChartJsLegend();
+  }
 
   ngOnInit() {
     this.incomeFormGroup = this._formBuilder.group({
       monthincctrl: ['', Validators.required],
       otherincctrl: ['']
     });
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
+    this.housingFormGroup = this._formBuilder.group({
+      phonectrl: [''],
+      cablectrl: [''],
+      waterctrl: [''],
+      repairctrl: [''],
+      homectrl: [''],
+      rentctrl: [''],
+      mortctrl: [''],
+      hoafeesctrl: ['']
     });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
+    this.transportFormGroup = this._formBuilder.group({
+      carctrl: [''],
+      carinsctrl: [''],
+      gasctrl: [''],
+      carrepctrl: ['']
+    });
+    this.educationalFormGroup = this._formBuilder.group({
+      schoolctrl: [''],
+      loansctrl: [''],
+      tuitionctrl: ['']
+    });
+    this.personalFormGroup = this._formBuilder.group({
+      groceriesctrl: [''],
+      clothingctrl: [''],
+      entertainctrl: [''],
+      medicalctrl: [''],
+      petctrl: [''],
+      otherctrl: [''],
+    });
+    this.savingsFormGroup = this._formBuilder.group({
+      emergctrl: [''],
+      investctrl: [''],
+      retirectrl: ['']
     });
   }
+  nextIncome() {
+    const monthlyincome: number = +this.incomeFormGroup.value.monthincctrl;
+    const otherincome: number = +this.incomeFormGroup.value.otherincctrl;
+    const totalincome: number = monthlyincome + otherincome;
+    this.pieChartData[0] = totalincome;
+    this.calculateTotalValue();
+  }
+  nextHousing() {
+    const mortgage: number = +this.housingFormGroup.value.mortctrl;
+    const hoafees: number = +this.housingFormGroup.value.hoafeesctrl;
+    const rent: number = +this.housingFormGroup.value.rentctrl;
+    const homeins: number = +this.housingFormGroup.value.homectrl;
+    const repair: number = +this.housingFormGroup.value.repairctrl;
+    const water: number = +this.housingFormGroup.value.waterctrl;
+    const cabletv: number = +this.housingFormGroup.value.cablectrl;
+    const phone: number = +this.housingFormGroup.value.phonectrl;
+    this.pieChartData[1] = mortgage + hoafees + rent + homeins + repair + water + cabletv + phone;
+    this.calculateTotalValue();
+  }
+  nextTransportation() {
+    const carpayment: number = +this.transportFormGroup.value.carctrl;
+    const carinsurance: number = +this.transportFormGroup.value.carinsctrl;
+    const gasfuel: number = +this.transportFormGroup.value.gasctrl;
+    const carrepairs: number = +this.transportFormGroup.value.carrepctrl;
+    this.pieChartData[2] = carpayment + carinsurance + gasfuel + carrepairs;
+    this.calculateTotalValue();
+  }
+  calculateTotalValue() {
+    this.totalValue = (+this.pieChartData[0]) -
+    ((+this.pieChartData[1]) + (+this.pieChartData[2])  + (+this.pieChartData[3]) +
+    (+this.pieChartData[4]) + (+this.pieChartData[5]));
+    console.log(this.pieChartData[0]);
+  }
+  nextEducation() {
+    const schoolsupp: number = +this.educationalFormGroup.value.schoolctrl;
+    const studloans: number = +this.educationalFormGroup.value.loansctrl;
+    const collegtuition: number = +this.educationalFormGroup.value.tuitionctrl;
+    this.pieChartData[3] = schoolsupp + studloans + collegtuition;
+    this.calculateTotalValue();
+  }
+  nextPersonal() {
+    const groceries: number = +this.personalFormGroup.value.groceriesctrl;
+    const clothing: number = +this.personalFormGroup.value.clothingctrl;
+    const entertainment: number = +this.personalFormGroup.value.entertainctrl;
+    const medical: number = +this.personalFormGroup.value.medicalctrl;
+    const petsupplies: number = +this.personalFormGroup.value.petctrl;
+    const otherexp: number = +this.personalFormGroup.value.otherctrl;
+    this.pieChartData[4] = groceries + clothing + entertainment + medical + petsupplies + otherexp;
+    console.log(this.pieChartData[4]);
+    console.log(groceries , clothing , entertainment , medical , petsupplies , otherexp);
+    this.calculateTotalValue();
 
+  }
+  SubmitValue() {
+      const finaldata =  {...this.incomeFormGroup.value, ...this.housingFormGroup.value,
+       ...this.transportFormGroup.value, ...this.educationalFormGroup.value, ...this.personalFormGroup.value,
+     ...this.savingsFormGroup.value};
+     console.log(finaldata);
+  }
+  nextSavings() {
+    const emergfund: number = +this.savingsFormGroup.value.emergctrl;
+    const investment: number = +this.savingsFormGroup.value.investctrl;
+    const retirement: number = +this.savingsFormGroup.value.retirectrl;
+    this.pieChartData[5] = emergfund + investment + retirement;
+    this.calculateTotalValue();
+
+  }
 }
